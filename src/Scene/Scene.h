c9667ce230852afcd2e73ed20d8944e058def164
CezaryKretkowski <73443812+CezaryKretkowski@users.noreply.github.com>
2022-05-04 07:37:13 +0200

    Camera,skybox,ObjLoader

---
+++ src/Scene/Scene.h
@@ -0,0 +1,22 @@
+//
+// Created by cezar on 16.04.2022.
+//
+
+#ifndef PGKPR_SCENE_H
+#define PGKPR_SCENE_H
+#include "Component.h"
+//class Frame;
+//class Scene:public Engine::Component{
+//public:
+//    void setUp(Engine::Frame super);
+//    void run(Engine::Frame super);
+//    void clean(Engine::Frame super);
+//
+//
+//    Scene(){}
+//    ~Scene(){}
+//    void virtual initScene(Engine::Frame super){ puts("initScene");}
+//    void virtual upDateScene(Engine::Frame super){ puts("upDateScene");}
+//    void virtual cleanScene(Engine::Frame super){puts("cleanScene");}
+//};
+#endif //PGKPR_SCENE_H
